19df99cb90fd54e04b250fad61a7aca86de08087
Pranjal <pjately078@gmail.com>
2019-12-18 14:32:54 +0000

    Connect to db

--- api/server.js
+++ api/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
+
+/* Load environment variables */
+dotenv.config({ path: './config/config.env' });
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-/* Load environment variables */
-dotenv.config({ path: './config/config.env' });
+/* Connect to db */
+connectDB();
 
 /* Dev logging middleware*/
 if (process.env.NODE_ENV === 'development') {
@@ -16,8 +20,14 @@ if (process.env.NODE_ENV === 'development') {
 /* Define routes*/
 app.use('/api/v1/users', require('./routes/users'));
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(
     `Server running in ${process.env.NODE_ENV} mode and listening on port ${PORT}`
   );
+});
+
+/* Handle unhandled promise rejections */
+process.on('unhandledRejection', error => {
+  console.error(error.message);
+  server.close(process.exit(1));
 });
