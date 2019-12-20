c3851eb779721095021396359f043ca07f05d73d
Pranjal <pjately078@gmail.com>
2019-12-20 12:23:51 +0000

    Register user endpoint with validation - needs to return JWT insetad

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,13 +1,59 @@
 const express = require('express');
+const User = require('../models/User');
+const bycrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
 const router = express.Router();
 
 /**
- * @route POST api/v1/users
- * @description Register a user
+ * @description Register user
+ * @route POST api/v1/users/register
  * @access Public
  */
-router.get('/', (req, res) => {
-  res.status(200).json({ success: 'Testing user endpoint...' });
-});
+router.post(
+  '/register',
+  [
+    check('name', 'Please enter a name')
+      .not()
+      .isEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res
+        .status(422)
+        .json({ success: false, msg: errors.array()[0].msg });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(422)
+          .json({ success: false, msg: 'Email already in use' });
+      }
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      user.password = await bycrypt.hash(password, 10);
+      await user.save();
+
+      res.status(200).send({ success: true });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).json({ success: false, msg: error.message });
+    }
+  }
+);
 
 module.exports = router;
