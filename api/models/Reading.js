3ec7c4a8d1cbbca47bd21acb3391db73a323e826
Pranjal <pjately078@gmail.com>
2019-12-20 18:25:29 +0000

    Reading schema and model

---
+++ api/models/Reading.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const readingSchema = gmongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'User cannot be blank'],
+  },
+  reading: {
+    type: Number,
+    required: [true, 'Reading cannot be blank '],
+    min: 0,
+  },
+  readingTime: {
+    type: Date,
+    required: [true, 'Reading Time cannot be blank'],
+  },
+});
+
+module.exports = mongoose.model('Reading', readingSchema);
