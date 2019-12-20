2b61507d8d344db639a692b844c802efe5ab04e7
Pranjal <pjately078@gmail.com>
2019-12-20 20:03:33 +0000

    Middleware checks if user exists (prevents against previously valid tokens that are now void i.e. user deleted)

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
-const auth = (req, res, next) => {
+const auth = async (req, res, next) => {
   const token = req.header('x-auth-token');
 
   if (!token) {
@@ -11,6 +12,10 @@ const auth = (req, res, next) => {
 
   try {
     let decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(req.userId);
+    if (!user) {
+      return res.status(404).json({ success: false, msg: 'User not found' });
+    }
     req.userId = decoded.id;
     next();
   } catch (error) {
