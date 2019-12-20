830553ba04eed8e08f3818edc8fbb5b27fd18963
Pranjal <pjately078@gmail.com>
2019-12-20 20:04:08 +0000

    Reading schema typo fix

--- api/models/Reading.js
+++ api/models/Reading.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const readingSchema = gmongoose.Schema({
+const readingSchema = mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
