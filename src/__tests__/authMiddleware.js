16b8d7d31a508449b068a2a64884fa3fc43cf814
Pranjal <pjately078@gmail.com>
2020-01-30 01:46:04 +0000

    clean up - removed describe block, used db helper functions and inclusion of object factory

--- src/__tests__/authMiddleware.js
+++ src/__tests__/authMiddleware.js
@@ -1,134 +1,113 @@
 const auth = require('../middleware/auth');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-const mongoose = require('mongoose');
+const { connectDb, resetDb, disconnectDb } = require('../config/db');
+const { buildNext, buildRes } = require('./utils/generate');
 
-const buildRes = overrides => {
-  const res = {
-    json: jest.fn(() => res).mockName('json'),
-    status: jest.fn(() => res).mockName('status'),
-    ...overrides,
+beforeAll(() => connectDb());
+
+afterAll(() => disconnectDb());
+
+beforeEach(() => resetDb());
+
+test('responds with 401 when no token present in header', () => {
+  const message = 'No token, authorisation denied';
+  const req = { header: jest.fn(() => null) };
+  const next = buildNext();
+  const res = buildRes();
+
+  auth(req, res, next);
+  expect(next).not.toHaveBeenCalled();
+  expect(req.header).toHaveBeenCalledWith('x-auth-token');
+  expect(req.header).toHaveBeenCalledTimes(1);
+  expect(res.status).toHaveBeenCalledWith(401);
+  expect(res.status).toHaveBeenCalledTimes(1);
+  expect(res.json).toHaveBeenCalledWith({ success: false, msg: message });
+  expect(res.json).toHaveBeenCalledTimes(1);
+});
+
+test('responds with 404 when user is not found', async () => {
+  const message = 'User not found';
+  jest.spyOn(jwt, 'verify');
+
+  jwt.verify.mockImplementation((token, secret) => {
+    return { id: 'incorrect_id' };
+  });
+
+  const req = {
+    header: jest.fn(() => true),
   };
-  return res;
-};
+  const next = buildNext();
+  const res = buildRes();
 
-const buildNext = () => jest.fn().mockName('next');
+  await auth(req, res, next);
+  expect(next).not.toHaveBeenCalled();
+  expect(req.header).toHaveBeenCalledWith('x-auth-token');
+  expect(req.header).toHaveBeenCalledTimes(1);
+  expect(res.status).toHaveBeenCalledWith(404);
+  expect(res.status).toHaveBeenCalledTimes(1);
+  expect(res.json).toHaveBeenCalledWith({ success: false, msg: message });
+  expect(res.json).toHaveBeenCalledTimes(1);
 
-describe('insert', () => {
-  let connection;
+  jwt.verify.mockRestore();
+});
 
-  beforeAll(async () => {
-    connection = await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-    });
+test('calls next when a valid token is included in the header and a user is found using decoded token', async () => {
+  const req = {
+    header: jest.fn(() => true),
+    userId: null,
+  };
+  const next = jest.fn();
+  const res = buildRes();
+
+  const user = new User({
+    name: 'John',
+    email: 'test@gmail.com',
+    password: '123456',
   });
 
-  afterAll(async () => await connection.disconnect());
+  await user.save();
 
-  beforeEach(async () => await User.deleteMany({}));
+  const insertedUser = await User.findOne({ name: user.name });
 
-  test('responds with 401 when no token present in header', () => {
-    const message = 'No token, authorisation denied';
-    const req = { header: jest.fn(() => null) };
-    const next = buildNext();
-    const res = buildRes();
+  jest.spyOn(jwt, 'verify');
 
-    auth(req, res, next);
-    expect(next).not.toHaveBeenCalled();
-    expect(req.header).toHaveBeenCalledWith('x-auth-token');
-    expect(req.header).toHaveBeenCalledTimes(1);
-    expect(res.status).toHaveBeenCalledWith(401);
-    expect(res.status).toHaveBeenCalledTimes(1);
-    expect(res.json).toHaveBeenCalledWith({ success: false, msg: message });
-    expect(res.json).toHaveBeenCalledTimes(1);
+  jwt.verify.mockImplementation((token, secret) => {
+    return { id: insertedUser._id };
   });
 
-  test('responds with 404 when user is not found', async () => {
-    const message = 'User not found';
-    jest.spyOn(jwt, 'verify');
+  await auth(req, res, next);
+  expect(req.header).toHaveBeenCalledWith('x-auth-token');
+  expect(req.header).toHaveBeenCalledTimes(1);
+  expect(res.status).not.toHaveBeenCalled();
+  expect(res.json).not.toHaveBeenCalled();
+  expect(next).toHaveBeenCalled();
 
-    jwt.verify.mockImplementation((token, secret) => {
-      return { id: 'incorrect_id' };
-    });
+  jwt.verify.mockRestore();
+});
 
-    const req = {
-      header: jest.fn(() => true),
-    };
-    const next = buildNext();
-    const res = buildRes();
+test('responds with 401 and error message when an invalid token is supplied', async () => {
+  const message = 'Invalid token';
+  jest.spyOn(jwt, 'verify');
 
-    await auth(req, res, next);
-    expect(next).not.toHaveBeenCalled();
-    expect(req.header).toHaveBeenCalledWith('x-auth-token');
-    expect(req.header).toHaveBeenCalledTimes(1);
-    expect(res.status).toHaveBeenCalledWith(404);
-    expect(res.status).toHaveBeenCalledTimes(1);
-    expect(res.json).toHaveBeenCalledWith({ success: false, msg: message });
-    expect(res.json).toHaveBeenCalledTimes(1);
-
-    jwt.verify.mockRestore();
+  jwt.verify.mockImplementation((token, secret) => {
+    throw Error();
   });
 
-  test('calls next when a valid token is included in the header and a user is found using decoded token', async () => {
-    const req = {
-      header: jest.fn(() => true),
-      userId: null,
-    };
-    const next = jest.fn();
-    const res = buildRes();
+  const req = {
+    header: jest.fn(() => true),
+  };
+  const next = buildNext();
+  const res = buildRes();
 
-    const user = new User({
-      name: 'John',
-      email: 'test@gmail.com',
-      password: '123456',
-    });
+  await auth(req, res, next);
+  expect(next).not.toHaveBeenCalled();
+  expect(req.header).toHaveBeenCalledWith('x-auth-token');
+  expect(req.header).toHaveBeenCalledTimes(1);
+  expect(res.status).toHaveBeenCalledWith(401);
+  expect(res.status).toHaveBeenCalledTimes(1);
+  expect(res.json).toHaveBeenCalledWith({ success: false, msg: message });
+  expect(res.json).toHaveBeenCalledTimes(1);
 
-    await user.save();
-
-    const insertedUser = await User.findOne({ name: user.name });
-
-    jest.spyOn(jwt, 'verify');
-
-    jwt.verify.mockImplementation((token, secret) => {
-      return { id: insertedUser._id };
-    });
-
-    await auth(req, res, next);
-    expect(req.header).toHaveBeenCalledWith('x-auth-token');
-    expect(req.header).toHaveBeenCalledTimes(1);
-    expect(res.status).not.toHaveBeenCalled();
-    expect(res.json).not.toHaveBeenCalled();
-    expect(next).toHaveBeenCalled();
-
-    jwt.verify.mockRestore();
-  });
-
-  test('responds with 401 and error message when an invalid token is supplied', async () => {
-    const message = 'Invalid token';
-    jest.spyOn(jwt, 'verify');
-
-    jwt.verify.mockImplementation((token, secret) => {
-      throw Error();
-    });
-
-    const req = {
-      header: jest.fn(() => true),
-    };
-    const next = buildNext();
-    const res = buildRes();
-
-    await auth(req, res, next);
-    expect(next).not.toHaveBeenCalled();
-    expect(req.header).toHaveBeenCalledWith('x-auth-token');
-    expect(req.header).toHaveBeenCalledTimes(1);
-    expect(res.status).toHaveBeenCalledWith(401);
-    expect(res.status).toHaveBeenCalledTimes(1);
-    expect(res.json).toHaveBeenCalledWith({ success: false, msg: message });
-    expect(res.json).toHaveBeenCalledTimes(1);
-
-    jwt.verify.mockRestore();
-  });
+  jwt.verify.mockRestore();
 });
