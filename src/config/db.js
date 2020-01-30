2ba3c752ce1609eddcb0b567bc7339d4882663a2
Pranjal <pjately078@gmail.com>
2020-01-30 01:43:29 +0000

    One file that provides helper functions for db operations

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,20 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  });
+module.exports = {
+  connectDb: async () => {
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
 
-  console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+
+    return conn;
+  },
+
+  resetDb: () => mongoose.connection.dropDatabase(),
+
+  disconnectDb: () => mongoose.connection.close(),
 };
-
-module.exports = connectDB;
